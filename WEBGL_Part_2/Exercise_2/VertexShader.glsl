d2e3ffbc1652ef7aacaf403c11ec9ce703b6dfbe
Iseli Nico niseli <nico.iseli@stud.hslu.ch>
2021-01-17 14:32:56 +0100

    Add all Exercises to repository

---
+++ WEBGL_Part_2/Exercise_2/VertexShader.glsl
@@ -0,0 +1,18 @@
+attribute vec2 aVertexPosition;
+
+// BLOCK 2 - AUFGABE 2 (COLORING)
+// attribute vec4 aVertexColor;
+// varying vec4 fragColor;
+
+
+// BLOCK 2 - AUFGABE 3 (TEXTURE MAPPING)
+attribute vec2 aVertexTextureCoord;
+varying vec2 vTextureCoord;
+
+void main () {
+    // BLOCK 2 - AUFGABE 2 (COLORING)
+    // fragColor = aVertexColor;
+
+    vTextureCoord = aVertexTextureCoord; // BLOCK 2 - AUFGABE 3 (TEXTURE MAPPING)
+    gl_Position = vec4(aVertexPosition, 0, 1);
+}
